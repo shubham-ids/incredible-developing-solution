f9578ff798dc7f66e39e7186b54cfbe0b0e29c76
rajinder singh <rajinder@incredible-developers.com>
2018-10-04 18:37:15 -0400

    modify the javascript validation and panding the email validation

--- footer.php
+++ footer.php
@@ -1,89 +1,39 @@
   </body>
   <script type="text/javascript">
     //Validation Requirment
-    function fnameRequired(fname){
-       if(fname.value === ""){ 
-         document.getElementById('firstName').style.backgroundColor = "pink";       
-         document.getElementById('fnameRequiredMessage').innerHTML = "First Name is required";
-       }else{
-         document.getElementById('fnameRequiredMessage').innerHTML = " ";
-         document.getElementById('firstName').style.backgroundColor = "white";
-       }
-    }
-    //Only words required Not a number
-    function fnameWordRequired(fnameWR){
-      if(!isNaN(fnameWR.value)){
-        document.getElementById('firstName').style.backgroundColor = "pink";
-        document.getElementById('fnameRequiredMessage').innerHTML = "<p color='red'>Enter the correct name<p>";
-       }else{
-         document.getElementById('fnameRequiredMessage').innerHTML = " ";
-         document.getElementById('firstName').style.backgroundColor = "white";
-       }  
-    }
-    //last Name Required
-    function lnameRequired(lname){
-       if(lname.value === ""){        
-          document.getElementById('lnameRequiredMessage').innerHTML = "Last Name is required";
-          document.getElementById('lastName').style.backgroundColor = "pink";
-       }else{
-          document.getElementById('lnameRequiredMessage').innerHTML = " ";
-          document.getElementById('lastName').style.backgroundColor = "white";
-        }  
-    } 
-    //Only words required Not a number
-    function lnameWordRequired(lnameWR){
-      if(!isNaN(lnameWR.value)){
-        document.getElementById('lastName').style.backgroundColor = "pink";
-        document.getElementById('lnameRequiredMessage').innerHTML = "Enter the correct name";
-       }else{
-         document.getElementById('lnameRequiredMessage').innerHTML = " ";
-         document.getElementById('lastName').style.backgroundColor = "white";
-       }  
+    /*
+    * This function is used to validation required field 
+    * this function which uesd two perameters 
+       => First perameter is used to given the input
+       => Second perameter is used to display the error message using HTML ID
+    * 
+    */
+  function validateRequired(input, errorId){
+    var errorElement = document.getElementById(errorId);
+    errorElement.innerHTML = "";
+    if(input.value === ""){
+       errorElement.innerHTML = "This field is required";
+       return;
     }    
-    //Phone NUmber Validation
-    // function phnumberRequired(lnameWR, errorId){
-    //  var ErrorMessageShow = document.getElementById(errorId);
-    //  var num  = "1234567890";    
-    //  for(var i=0; i<lnameWR.length; i++){
-    //   console.log(lnameWR.length);
-    //  }   
-    //  return;   
-    // }  
-   // valide phone number validation
-    function valideNumber(input){
-      var allowedCharacter = ["+","-","0","1","2","3","4","5","6","7","8","9"];
-
-      var value = input.value;
-
-      console.log(value, value.length);
-      for(var i=0; i < value.length; i++){
-           //console.log(values.value.length);
-       if( allowedCharacter.indexOf(value[i]) == -1  ){
-         console.log('Please enter the correct number')
-          return false;
-       }
-
-      return true;
-
-    }
-
-
-    // if(input.value === ""){
-    //   return errorElement.innerHTML = "This field is required";
-    // }   
-    // if(input.value == upperCaseArray){
-    //   return errorElement.innerHTML = "Please enter the only numbers";
-    // } 
-    // if(input.value.length < 10){     
-    //   return errorElement.innerHTML = "Only 10 numbers are allowed";
-    // }  
-    // if(input.value.length > 10){    
-    //   return errorElement.innerHTML = "Please erase the extra number";
-    // }  
   }
-     //Email validation Eroor
 
-       /*
+  /*
+  * The function is used to  display the blank error validation
+  *
+  *
+  */
+   function genderRequired(value , errorId){
+    var errorElement = document.getElementById(errorId);
+    errorElement.innerHTML = "";
+    var maleId  = document.getElementById('maleRadio').checked;
+    var fmaleId = document.getElementById('femaleRadio').checked;
+    var otherId = document.getElementById("otherRadio").checked;
+    if( maleId == true || fmaleId == true || otherId == true){
+      return;
+    }
+    errorElement.innerHTML = "This field is required";
+   }
+    /*
    *
    * Parameter : 
    *  value    : Input string+
@@ -92,15 +42,159 @@
    *    true  : If it has a single uppercase character
    *    false : If it has no uppercase character
    */
-  /*function emailSpicalCharacterMatch(input , ID){
-    var spicalCharacter = "@ .";
-    //console.log(input.value.length);
-    for(var i=0; i < spicalCharacter.length; i++ ){
-      console.log(spicalCharacter[i]);
-      if(input.value.indexOf(spicalCharacter[i]) !== spicalCharacter[i]){
-        console.log('Please Enter the First word like : [A to Z]');
+  function findUpperCaseWithCustomMatching(value){
+    var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var testString = value;
+    var counter = 0;
+    var flag = false;
+    for(var i= 0 ; i < testString.length; i++){
+      for(var y=0; y < upperCaseArray.length; y++){
+        if(testString[i] == upperCaseArray[y] ){
+          console.log('we found one upper case i.e' , upperCaseArray[y] );
+          flag = true;
+          break;
+        }
+      }
+      if(flag == true){
+        break;
       }
     }
-  }*/
+    return flag;
+  }
+  /*
+   *
+   * Parameter : 
+   *  value    : Input string+
+   * 
+   *  Return   : boolean
+   *    true  : If it has a single special character
+   *    false : If it has no special character
+   */
+  function findSpecialCharacterWithCustomMatching(value){
+    var specialCharacters = "~!@#$%^&*()_+{}:|<>?";
+    for(var i= 0 ; i < value.length; i++){
+      if(specialCharacters.indexOf(value[i]) !== -1){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /*
+  * Correct Email validation Error
+  * shjd@fnghlkf.khdfg
+  */
+  function emailRequired(input ,errorId ){
+    var specialCharacters = "@.";
+    var EmailField = input.value;
+    var emailElement = document.getElementById(errorId);
+    emailElement.innerHTML = "";    
+    if(specialCharacters.indexOf(EmailField[i=0]) !== -1){
+      
+      emailElement.innerHTML = "Invalide Email";
+      return; 
+    }
+    if(EmailField[i=0] !== '@'){
+      console.log( input.value);
+      for(var i=0; i<input.value.length; i++){
+        for(var j=0; j<specialCharacters.length; j++){
+           console.log(input.value[i] +"Matched"+specialCharacters[j]);
+           if(input.value[i] !== specialCharacters[j])  {
+              emailElement.innerHTML = "Invalide Email";
+           }
+        }
+      }
+    }
+    return false;
+  }
+
+/*
+* This function is used to two perameter
+* Checked the blank filed
+* Checked the Min and Max value
+* Find the Uppercase value 
+* Find the Special character 
+*/
+    function passwordValidation(input, errorId){
+      var errorElement = document.getElementById(errorId);
+      errorElement.innerHTML = "";
+      if(input.value === ""){
+        return errorElement.innerHTML = "This field is required";
+      }    
+      if(input.value.length < 8){
+        return  errorElement.innerHTML = "Min character allowed are 8";
+      }
+      if(input.value.length > 15){
+        return errorElement.innerHTML = "Max character allowed are 15";
+      }  
+      if(findUpperCaseWithCustomMatching(input.value) === false){
+        return  errorElement.innerHTML = "At least one Uppercase letter";
+      }
+      if(findSpecialCharacterWithCustomMatching(input.value) === false){
+        return errorElement.innerHTML = "Atleast one special character";
+      }
+   }
+  /*
+  * Confirm password validation
+  * Matching Password and confirm password
+  */
+  function conpass(){
+    cPasswordTouched = true; 
+    var errorElement = document.getElementById('confirmPasswrdRequiredMessage');
+    var password     = document.getElementById("password");
+    var cPassword    = document.getElementById("Cpassword");
+    errorElement.innerHTML = "";
+    if(password.value != cPassword.value)
+    {
+      errorElement.innerHTML = "Password not match";
+    }
+  }   
+
+  /*
+   *
+   *
+   * Function name :  modifiedNumber 
+   * Parmater      : value ( string )
+   * return        : boolean
+    -- True  -- In case all letters are number 
+    -- False -- In case any one letter is not a number
+   */
+
+  function modifiedNumber(value){
+    var allowedCharacter = ["+","-","0","1","2","3","4","5","6","7","8","9"];
+    for(var i=0; i< value.length; i++){
+     if( allowedCharacter.indexOf(value[i]) == -1  ){
+        return false;
+     }
+    }
+    return true;
+  }
+
+  /*
+  * Mobile validation
+  * Required
+  *Min or max number allowed
+  *charachter are not allowed
+  */
+  function mobileRequired(input, errorId){
+    var errorElement = document.getElementById(errorId);
+    errorElement.innerHTML = "";
+    if(input.value === ""){
+      errorElement.innerHTML = "This field is required";
+      return;
+    }    
+    if(modifiedNumber(input.value) === false){
+      errorElement.innerHTML = "Only numbers are allowed";
+      return;
+    }
+    if(input.value.length < 11){
+      errorElement.innerHTML = "Min Numbers allowed are 10";
+      return;
+    }  
+    if(input.value.length > 11){
+      errorElement.innerHTML = "Please erase the extra add number ";
+      return;
+    }  
+  }    
   </script>
   </html>
--- register.php
+++ register.php
@@ -21,7 +21,8 @@
                     <label for="firstName" class="col-sm-3 control-label">First Name</label>
                     <div class="col-sm-9">
                         <input 
-                        onkeyup="findUpperCaseWithCustomMatching(this)" 
+                        onkeyup="validateRequired(this , 'fnameRequiredMessage')" 
+                        onblur="validateRequired(this , 'fnameRequiredMessage')" 
                         type="text" id="firstName" name="fname " placeholder="First Name" class="form-control" autofocus>
                         <div id="fnameRequiredMessage" style="color: red">
                         	<?php
@@ -35,43 +36,93 @@
                 <div class="form-group">
                     <label for="lastName" class="col-sm-3 control-label">Last Name</label>
                     <div class="col-sm-9">
-                        <input onkeypress="" type="text" id="lastName" name="lname" placeholder="Last Name" class="form-control" autofocus>
+                        <input
+                        onkeyup="validateRequired(this , 'lnameRequiredMessage')" 
+                        onblur="validateRequired(this , 'lnameRequiredMessage')" 
+                         type="text" id="lastName" name="lname" placeholder="Last Name" class="form-control" autofocus>
                         <div id="lnameRequiredMessage" style="color: red"></div>                        
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="email" class="col-sm-3 control-label">Email* </label>
                     <div class="col-sm-9">
-                        <input onkeypress="//emailSpicalCharacterMatch(this , emailRequiredMessage);//emailRequired(this)" type="email" id="email" name="email" placeholder="Email" class="form-control" name= "email">
-                        <div id="emailRequiredMessage"></div>
+                        <input 
+                         onkeyup="validateRequired(this ,'emailRequiredMessage'); emailRequired(this , 'emailRequiredMessage');"
+                         onblur="validateRequired(this ,'emailRequiredMessage'); emailRequired(this , 'emailRequiredMessage');"
+                         type="email" id="email" name="email" placeholder="Email" class="form-control" name= "email">
+                        <div id="emailRequiredMessage" style="color: red"></div>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="password" class="col-sm-3 control-label">Password*</label>
                     <div class="col-sm-9">
-                        <input onkeypress="passwordRequired(this)" type="password" id="password" name="password" placeholder="Password" class="form-control">
+                        <input 
+                        onkeyup="passwordValidation(this ,'passwrdRequiredMessage');" 
+                        onblur="passwordValidation(this ,'passwrdRequiredMessage');" 
+                        type="text" id="password" name="password" placeholder="Password" class="form-control">
                         <div id="passwrdRequiredMessage" style="color: red"></div> 
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="password" class="col-sm-6 control-label">Confirm Password*</label>
                     <div class="col-sm-9">
-                        <input onkeypress="cnfrmpasswrd(this)" type="password" id="Cpassword" placeholder="Password" class="form-control">
-                        <div id="confirmPasswrdRequiredMessage"></div> 
+                        <input 
+                        onkeyup="validateRequired(this ,'confirmPasswrdRequiredMessage'); conpass();" 
+                        onblur="validateRequired(this ,'confirmPasswrdRequiredMessage'); conpass();""
+                        type="text" id="Cpassword" placeholder="Password" class="form-control">
+                        <div id="confirmPasswrdRequiredMessage" style="color: red"></div> 
                     </div>
                 </div>                
                 <div class="form-group">
                     <label for="phoneNumber" class="col-sm-4 control-label">Phone number </label>
                     <div class="col-sm-9">
-                        <input 
-                          onkeyup="valideNumber(this)"
-                          type="text" id="phoneNumber" name="phnumber " placeholder="Phone number" class="form-control">
+                        <input
+                         onkeyup ="mobileRequired(this , 'phoneNumberRequiredMessage')"
+                         onblur  ="mobileRequired(this , 'phoneNumberRequiredMessage')" 
+                        type="text" id="phoneNumber" name="phnumber " placeholder="Phone number" class="form-control">
                         <div id="phoneNumberRequiredMessage" style="color: red"></div>
                         <div id="phonenumberLengthMessage" style="color: red"></div>
-                      <!--  <span class="help-block">Your phone number won't be disclosed anywhere </span> -->
+                        <span class="help-block">Phone Number Like: +789456123 </span>
                     </div>
                 </div>
-                <button type="submit" name="add" class="btn btn-primary btn-block">Register</button>
+                <div class="form-group">
+                    <label class="control-label col-sm-3">Gender</label>
+                    <div class="col-sm-12">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <label class="radio-inline">
+                                    <input
+                                    onblur="genderRequired(this , 'GenderErrorMessage')" 
+                                    onkeyup="genderRequired(this , 'GenderErrorMessage')" 
+                                    type="radio" id="maleRadio" name="gender" value="Male">Male
+                                </label>
+                            </div>
+                            <div class="col-sm-4">
+                                <label class="radio-inline">
+                                    <input
+                                    onblur="genderRequired(this , 'GenderErrorMessage')" 
+                                    onkeyup="genderRequired(this , 'GenderErrorMessage')"                      
+                                     type="radio" id="femaleRadio" name="gender" value="Female">Female
+                                </label>
+                            </div>
+                            <div class="col-sm-4">
+                                <label class="radio-inline">
+                                    <input
+                                    onblur="genderRequired(this , 'GenderErrorMessage')" 
+                                    onkeyup="genderRequired(this , 'GenderErrorMessage')"                      
+                                    type="radio" id="otherRadio" name="gender" value="Other">Others
+                                </label>
+                            </div>  
+                            <div id="GenderErrorMessage" style="color: red"></div>                          
+                        </div>
+                    </div>
+                </div> <!-- /.form-group -->
+                <div class="form-group">
+                    <div class="col-sm-9 col-sm-offset-3">
+                        <span class="help-block">*Required fields</span>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Register</button>
             </form> <!-- /form -->
         </div> <!-- ./container -->
 </form>
