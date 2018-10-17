08de27803162078d905e6fcce9ccf17e9da6fcf9
shubham-ids <shubham@incredible-developers.com>
2018-10-17 16:41:13 +0530

    Complete the task for calculator and learning the ajax call

---
+++ calculator/calculator.php
@@ -0,0 +1,146 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Calculator</title>
+    <link rel="stylesheet" href="../css/custom-calculator.css" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <script src="../includes/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="Container">
+      <div class="Header">
+        <h1>Calculator</h1>
+      </div>
+      <div class="outputContainer">0</div>
+      <div class="btnContanier">
+        <div class="btnContant">
+          <button class="InputNumber" value="CE">CE</button>
+          <button class="InputNumber" value="(">(</button>
+          <button class="InputNumber" value=")">)</button>
+          <button class="InputNumber" value="%">%</button>
+
+          <button class="Number" value="7">7</button>
+          <button class="Number" value="8">8</button>
+          <button class="Number" value="9">9</button>
+          <button class="InputNumber" value="*">x</button>
+
+          <button class="Number" value="4">4</button>
+          <button class="Number" value="5">5</button>
+          <button class="Number" value="6">6</button>
+          <button class="InputNumber" value="-">-</button>
+
+          <button class="Number" value="1">1</button>
+          <button class="Number" value="2">2</button>
+          <button class="Number" value="3">3</button>
+          <button class="InputNumber" value="+">+</button>
+
+          <button class="Number" value="0">0</button>          
+          <button class="InputNumber" value=".">.</button>
+          <button class="InputNumber" value="=">=</button>
+        </div>           
+      </div>      
+    </div>
+    <script type="text/javascript">
+      $(document).ready(function(){
+        console.log($("#outputContainer").text());
+        var firstOperand, // Contain the first operand
+         operator, // contain the selector operator
+         resetNumber = false; // Why this is used very important ????
+
+  
+        //This method is used to append the button value
+        $('.Number').click(function(){
+          if(resetNumber){
+            $('.outputContainer').html("");
+            resetNumber = false;
+           }
+          $('.outputContainer').append( $(this).val() );
+        });  
+        //This method is used to erace the text value one by one
+        $('.InputNumber[value="CE"]').click(function(){
+         var textValue = $('.outputContainer').text(); // This method is used to show the output text value
+         var EraceString = textValue.slice(0 , -1);    // This method is used to erace the text value
+         if( EraceString == 0 ){
+            EraceString = "0";
+         }
+         $('.outputContainer').text(EraceString);
+        });
+        // This method is used to addtion of the  value
+        $('.InputNumber[value="+"]').click(function(){
+          firstOperand = $('.outputContainer').text();
+          operator = "+";
+          resetNumber = true;
+        });
+        // This method is used to addtion of the  value
+        $('.InputNumber[value="-"]').click(function(){
+          firstOperand = $('.outputContainer').text();
+          operator = "-";
+          resetNumber = true;
+        });        
+
+        // This method is used to multiplication of the  value
+        $('.InputNumber[value="*"]').click(function(){
+          firstOperand = $('.outputContainer').text();
+          operator = "*";
+          resetNumber = true;
+        }); 
+
+        // This method is used to store os the value
+        $('.InputNumber[value="="]').click(function(){
+          secondOperand = $('.outputContainer').html();
+
+          console.log( parseInt(firstOperand), operator, parseInt(secondOperand));
+          console.log( parseInt(firstOperand) +operator+ parseInt(secondOperand));
+
+          // var calculation = eval( parseInt(firstOperand) +operator+ parseInt(secondOperand) );
+          // $('.outputContainer').html( calculation );
+          // return;
+          console.log(operator);
+          operatorvalue = operator;
+
+          var a = parseInt(firstOperand);
+          var b = parseInt(secondOperand);
+          var c;
+
+          switch(operatorvalue){
+            case "+":
+              c = a+b; 
+            break;
+            case "-":
+              c = a-b; 
+            break; 
+            case "*":
+              c = a*b; 
+            break;                 
+            default:
+              c = "Something went wrong, pelase refresh the page.";  
+          }
+
+          $('.outputContainer').html( c );
+  
+
+
+
+
+
+
+          // //This method is used to addtion of the value
+          // if( $('.InputNumber[value="+"]').val() == "+" ){
+          //   var a = parseInt(firstOperand);
+          //   var b = parseInt(secondOperand);
+          //   var c = a+b; 
+          //   $('.outputContainer').html( c );
+          // }
+          // // This method is used to subtraction of the value
+          // else if( $('.InputNumber[value="-"]').val() == "-" ){
+          //   var a = parseInt(firstOperand);
+          //   var b = parseInt(secondOperand);
+          //   var c = a-b; 
+          //   $('.outputContainer').html( c );
+          // }
+        });
+
+      });
+    </script>
+  </body>
+</html>
