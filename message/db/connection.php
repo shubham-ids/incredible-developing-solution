dd9eff8a5af8e8f6940d516585652281a3822655
shubham-ids <shubham@incredible-developers.com>
2018-10-18 19:05:14 +0530

    New task  from message passing

---
+++ message/db/connection.php
@@ -0,0 +1,14 @@
+<?php
+  define('HOST','localhost');
+  define('USERNAME','root');
+  define('PASSWORD','root');
+  define('IDS','ids');
+  define('RECORD','record');
+  // Database connectivity
+  $Mysqli = new mysqli(HOST,USERNAME,PASSWORD,IDS);
+  if( $Mysqli ){
+  	echo "connection is successfull";
+  }else{
+  	 echo $mysqli->connect_error(); // Display the error in database not connectivity
+  	}
+?>
